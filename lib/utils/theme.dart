7849ac24c0bf90dd9bb33371a18b048c17d83dbf
Timilehin <timilehintimothy.jegede@gmail.com>
2021-03-18 22:40:12 +0100

    chore: setup project and fetched assets 🦾.

---
+++ lib/utils/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const greyColor = Color(0xFF909090);
+const darkPurpleColor = Color(0xFF7B4397);
+const purpleColor = Color(0xFF9F5DE2);
+const greenColor = Color(0xFF0CB8B6);
---
+++ lib/views/screens/medications/medication_detail_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MedicationDetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
